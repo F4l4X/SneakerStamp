c71ffcc2563cb7105af0fe96948801173aa38d96
Enrique <73123851+F4l4X@users.noreply.github.com>
2023-04-22 22:40:54 +0200

    basic structure done

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as Sneakers } from './Sneakers';
+export { default as SneakersDetails } from './SneakersDetails';
